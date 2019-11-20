52df120eacd69f6c4055bb9a0a2ce72b46e9b5cf
niziming <zimingnee@163.com>
2019-11-20 10:08:28 +0800

    统一消息处理

--- src/main.js
+++ src/main.js
@@ -14,9 +14,11 @@ import axios from 'axios'
 import '../static/css/global.css'
 import eleCalendar from 'ele-calendar'
 import less from 'less'
+import { handleErrorMsg } from '@/common/common'
 // import 'ele-calendar/dist/vue-calendar.css'
 Vue.prototype.$axios = axios
 
+const AUTH_TOKEN = sessionStorage.getItem('Authorization')
 axios.defaults.baseURL = '/api'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 Vue.config.productionTip = false
@@ -26,6 +28,52 @@ Vue.use(Vuex)
 Vue.use(ElementUI)
 Vue.use(eleCalendar)
 Vue.use(less)
+
+// 全局请求头处理
+axios.interceptors.request.use((config) => {
+  // console.log('main-config')
+  // console.log(config)
+  // 请求前面请求数据用动画带起,服务方式调用
+  // // ****************************
+  // startLoading()
+  // endLoading()
+  // // ****************************
+  return config
+}, function (err) {
+  // 对请求错误的处理
+  console.log('发送失败')
+  console.log(err)
+  // // ****************************
+  // startLoading()
+  // endLoading()
+  // ****************************
+  return Promise.reject(err)
+})
+
+// 全局响应头处理
+axios.interceptors.response.use((config) => {
+  // console.log('response-main-config')
+  // console.log(config.data.code)
+  // console.log(handleErrorMsg(config.data.code))
+  // ****************************
+  // startLoading()
+  // endLoading()
+  // ****************************
+  if (config.data.code === 200) {
+    // endLoading()
+    return config
+  }
+  ElementUI.Message.warning(handleErrorMsg(config.data.code))
+  return config
+}, function (err) {
+  // console.log('main-err')
+  // console.log(err)
+  // ****************************
+  // startLoading()
+  // endLoading()
+  // ****************************
+  return Promise.reject(err)
+})
 
 /* eslint-disable no-new */
 // 实例化 router store
